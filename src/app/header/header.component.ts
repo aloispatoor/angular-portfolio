988a4a2204f89c7a734bdf09c18171bc24472676
Alois Patoor <palois89@posteo.net>
2022-10-03 14:00:01 +0200

    New Angular project to change the style files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+  isFixedNavbar !: boolean;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.menuBurger();
+    this.onWindowScroll();
+  }
+
+  @HostBinding('class.navbar-opened') navbarOpened = false;
+  menuBurger(){
+    this.navbarOpened = !this.navbarOpened;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const desktopNavbar = document.querySelector('.desktopNav');
+    const sticky = desktopNavbar?.offsetTop;
+    if(scrollY >= sticky){
+      this.isFixedNavbar = true;
+    } else {
+      this.isFixedNavbar = false;
+    }
+  }
+
+  onContinue(): void {
+    this.router.navigateByUrl('');
+  }
+
+}
